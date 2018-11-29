1fe047b335519bd53920c8fa0cc5a20522c96b76
Semeriak <44653025+Semeriak@users.noreply.github.com>
2018-11-29 19:51:10 +0200

    Add files via upload

---
+++ 11111петро.js
@@ -0,0 +1,9 @@
+<script>
+alert("Zavd.1");
+function cube (number){
+	alert(number*number*number)
+var number;
+ number=prompt("VVedit nomer vashoho varianty");
+ alert("Vach variant u tretjomu stepeni = " +cube(number));
+ </script>
+
