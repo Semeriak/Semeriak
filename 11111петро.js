8cf4d77a38a3ecb5bdec6d44abfba8b237d375c7
Semeriak <44653025+Semeriak@users.noreply.github.com>
2018-11-29 19:52:02 +0200

    Delete 11111петро.js

--- 11111петро.js
+++
@@ -1,9 +0,0 @@
-<script>
-alert("Zavd.1");
-function cube (number){
-	alert(number*number*number)
-var number;
- number=prompt("VVedit nomer vashoho varianty");
- alert("Vach variant u tretjomu stepeni = " +cube(number));
- </script>
-
